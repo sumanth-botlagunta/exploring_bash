c61fc2822b1c82304df6cfbb57392bcf0172c99c
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2024-06-11 20:49:28 +0530

    hello world

---
+++ hello_world.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=$(whoami)
+language=$1
+date=$(date)
+curr_dir=$(pwd)
+
+echo "Hi I am $name"
+sleep 1
+echo "I am Learning $language"
+sleep 1
+echo "now the date is $date"
+sleep 1
+echo "this script is running at $curr_dir"
+
+
