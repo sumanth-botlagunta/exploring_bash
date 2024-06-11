b1e04c45d020e39597c9facf906a681682bc4375
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2024-06-11 20:57:42 +0530

    added a line

--- hello_world.sh
+++ hello_world.sh
@@ -9,6 +9,8 @@ echo "Hi I am $name"
 sleep 1
 echo "I am Learning $language"
 sleep 1
+echo "this is my first $language script"
+sleep 1
 echo "now the date is $date"
 sleep 1
 echo "this script is running at $curr_dir"
